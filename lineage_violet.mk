ae28643b56a0a66292acec896e8f0e9fa4204316
GoZiko <anirban89m@gmail.com>
2025-04-24 15:44:40 +0530

    Update lineage_violet.mk @GenesisOS BringUp

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -13,11 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common Genesis OS  Stuff.
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
+# GenesisOs
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+GENESIS_OFFICIAL := false
+PRODUCT_NAME := genesis_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
