a7a1893f93c7d6809adb6d1e8fa54fb294d741f5
GoZiko <anirban89m@gmail.com>
2025-04-26 00:46:09 +0530

    Update AndroidProducts.mk Droid-X BringUp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,9 @@
-#
-# Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
+ 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixelage_violet.mk
+    $(LOCAL_DIR)/droidx_violet.mk
+ 
+COMMON_LUNCH_CHOICES := \
+    droidx_violet-user \
+    droidx_violet-userdebug \
+    droidx_violet-eng
