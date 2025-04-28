64120bae29c156d0848101951810ebf49af9affc
GoZiko <anirban89m@gmail.com>
2025-04-28 17:01:40 +0530

    alpha_violet.mk BringUp for AlphaDroid

--- alpha_violet.mk
+++ alpha_violet.mk
@@ -13,8 +13,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common AlphaDroid stuff
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+# Device Specific
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+ 
+
+# Launcher
+TARGET_INCLUDE_LAWNCHAIR := true
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+# Extras
+TARGET_INCLUDE_RIMUSIC := true
+# Maintainer
+ALPHA_BUILD_TYPE := UnOfficial
+ALPHA_MAINTAINER := Anirban
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
